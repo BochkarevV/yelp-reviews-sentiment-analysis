fbdd3e20db2e49759cda8c74365d6a208061512a
vasili.bocicariov <bochkarevv@gmail.com>
2020-05-27 22:46:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Review Ratings Prediction
+
+## Task
+In this project we will observe the power of BERT. The task is to create an NLP model 
+that would be able to predict rating score (`stars`) users assign to restaurants based 
+on their comments.
+
+## Data
+We'll use YELP reviews for this task. Original Yelp Open Dataset is available 
+[here](https://www.yelp.com/dataset).
+
+In order to reduce the training time and avoid using 
+expensive and often unavailable hardware, we'll extract the first 100,000 records and
+split them into training, validation and test sets for development and final model 
+evaluation.  
+
+## Repository Structure
+* `data` - contains the data we're using for dev and testing purposes.
+* `models` - models definitions.
+* `notebooks` - auxiliary notebooks, such as data exploration and example of 
+the model usage.
+* `utils` - helpers and supplementary methods, such as first N records extraction from 
+the original dataset.
+* `main.py` - default root of the project, not used at the moment.
+* `README.py` - the doc you're reading :)
+* `requirements.txt` - project dependencies.
+
+## Model
+
+
+
+## ToDo
+* Add other models for comparison, e.g. Bag-of-Words, RNNs.
---
+++ utils/data_utils.py
@@ -0,0 +1,60 @@
+# %%
+import os
+import json
+import zipfile
+import pandas as pd
+
+
+# %%
+def fetch_per_category(n, path=os.path.join('data', 'yelp_academic_dataset_review.json.zip')):
+    """
+    Fetch the first N reviews per star rating from the YELP reviews dataset.
+    The resulting set will consist of 5*N records.
+
+    :param n: number per star rating of records to fetch.
+    :param path: path to the source YELP reviews .json.zip archive.
+    :return: list of YELP review objects.
+    """
+
+    subsample = []
+    counts = {}
+
+    # Read zipped JSON
+    with zipfile.ZipFile(path, 'r') as z:
+        for filename in z.namelist():
+            with z.open(filename) as f:
+
+                # Iterate over the reviews
+                for line in f:
+                    review = json.loads(line.decode('utf-8'))
+
+                    # Collect records and update the count
+                    if review['stars'] not in counts:
+                        subsample.append(review)
+                        counts[review['stars']] = 1
+                    elif counts[review['stars']] < n:
+                        subsample.append(json.loads(line.decode('utf-8')))
+                        counts[review['stars']] += 1
+
+                    # Break when n records are gathered for all star ratings
+                    if all(c == n for c in counts.values()) == n:
+                        break
+
+    return subsample
+
+
+def save_subsample(dataset, path=os.path.join('data', 'yelp_reviews.json.gz')):
+    """
+    Save YELP data subsample into a compressed JSON.
+
+    :param dataset: list of YELP review objects.
+    :param path: path to save the subsample to.
+    """
+
+    df = pd.DataFrame(dataset)
+    df.to_json(path, orient='records', compression='gzip', lines=True)
+
+
+# %%
+data = fetch_per_category(25_000)
+save_subsample(data)
